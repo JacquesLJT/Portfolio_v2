546d96852124fe3343a65f3115100a1c2eccf9fa
Landon Thibodeau <landonthib@gmail.com>
2023-05-24 19:24:26 -0400

    form change

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,66 +1,79 @@
 <template>
-  <form @submit.prevent="submitForm" name="contact" method="POST" data-netlify="true">
-    <label for="name">Name</label>
-    <input type="text" id="name" v-model="name" required />
+  <c-box 
+    as="form" 
+    data-netlify="true" 
+    name="contact" 
+    method="POST" 
+    @submit.prevent="submitForm" 
+    p="6" 
+    shadow="2xl" 
+    borderWidth="1px" 
+    bg="white"
+    rounded="lg"
+  >
+    <c-form-control my="5">
+      <c-form-label for="name">Your Name</c-form-label>
+      <c-input id="name" type="text" v-model="form.name" required />
+    </c-form-control>
 
-    <label for="email">Email</label>
-    <input type="email" id="email" v-model="email" required />
+    <c-form-control my="5">
+      <c-form-label for="email">Your Email</c-form-label>
+      <c-input id="email" type="email" v-model="form.email" required />
+    </c-form-control>
 
-    <label for="message">Message</label>
-    <textarea id="message" v-model="message" required></textarea>
+    <c-form-control my="5">
+      <c-form-label for="message">Your Message</c-form-label>
+      <c-textarea id="message" v-model="form.message" required />
+    </c-form-control>
 
-    <button type="submit">Submit</button>
-  </form>
+    <c-button type="submit" colorScheme="blue" width="100%">Submit</c-button>
+  </c-box>
 </template>
 
 <script>
+import { CBox, CFormControl, CFormLabel, CInput, CTextarea, CButton } from '@chakra-ui/vue';
+
 export default {
+  components: {
+    CBox,
+    CFormControl,
+    CFormLabel,
+    CInput,
+    CTextarea,
+    CButton
+  },
+
   data() {
     return {
-      name: '',
-      email: '',
-      message: '',
+      form: {
+        name: '',
+        email: '',
+        message: '',
+      },
     };
   },
   methods: {
-    submitForm() {
-      const formData = {
-        name: this.name,
-        email: this.email,
-        message: this.message,
+    async submitForm() {
+      const encode = (data) => {
+        return Object.keys(data)
+          .map(
+            (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+          )
+          .join('&');
       };
-      // You can handle the form submission here
-      console.log(formData);
+
+      await fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+          'form-name': 'contact',
+          ...this.form,
+        }),
+      })
+        .then(() => alert('Form successfully submitted!'))
+        .catch((error) => alert(error));
     },
   },
 };
 </script>
 
-<style scoped>
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input,
-textarea {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-}
-
-button[type="submit"] {
-  width: 100%;
-  background-color: #101935;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-}
-
-button[type="submit"]:hover {
-  /* increase transparancy of background */
-  background-color: rgba(16, 25, 53, 0.8);
-}
-</style>
